71d72ea675bb359fa5143e0698bd53f73fbb9e9a
Taras-Neridnyi <t.neridniy@gmail.com>
2025-11-23 20:09:21 +0200

    done day 6

--- Days/Day5.py
+++ Days/Day5.py
@@ -6,7 +6,7 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-f
+
 password_list = []
 
 for char in range(1, nr_letters + 1):
